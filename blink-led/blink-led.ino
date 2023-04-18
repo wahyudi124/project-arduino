e43e360b02933cc816929eec0c2748a337130dd5
BIMA-25 <bima.bagus.25@gmail.com>
2023-04-18 14:40:57 +0700

    tambah led 3

--- blink-led/blink-led.ino
+++ blink-led/blink-led.ino
@@ -1,6 +1,8 @@
 void setup(){
     pinMode(13,OUTPUT);
     pinMode(14, INPUT);
+     pinMode(27,OUTPUT);
+    
 }
 
 
@@ -8,5 +10,6 @@ void setup(){
 void loop(){
     if(digitalRead(14) == 0){
         digitalWrite(13, HIGH);
+        digitalWrite(27, HIGH);
     }
 }
